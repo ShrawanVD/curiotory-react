1c5a4e6d353edde934675135bb73c3aa6aacebfb
ShrawanVD <shra1.deo7714@gmail.com>
2024-06-20 11:24:28 +0530

    modified 404 page in more friendly manner and also made optimisations in the webiste so made changes in the index.html file as well.

--- src/pages/NotFound/NotFoundDev.jsx
+++ src/pages/NotFound/NotFoundDev.jsx
@@ -3,8 +3,6 @@ import not from "./NotFound.module.css";
 
 const NotFoundDev = ({ message = "Page Under Development" }) => (
 
-  // 
-
   <div>
     <div className={not.error_page}>
       <div className={not.container1}>
@@ -20,14 +18,25 @@ const NotFoundDev = ({ message = "Page Under Development" }) => (
           <h1 className={not.error_page__heading_title}>{message}</h1>
           <p className={not.error_page__heading_desciption}>404 error</p>
         </div>
-        <a
-          className={not.error_page__button}
-          href="/"
-          aria-label="back to home"
-          title="back to home"
-        >
-          back to home
-        </a>
+        <p className={not.error_page__heading_description} style={{
+          marginLeft:"1rem",
+          marginRight:"1rem"
+        }}>
+          Sorry, the page you are looking for is currently under Development. Here are some helpful links:
+        </p>
+        <div className={not.row}>
+        <a className={not.error_page__button} href="/" aria-label="back to home" title="back to home">Home</a>
+        <a className={not.error_page__button} href="/aboutus" aria-label="about us" title="about us">About Us</a>
+        <a className={not.error_page__button} href="/contactus" aria-label="contact us" title="contact us">Contact Us</a>
+        <a className={not.error_page__button} href="/blogs" aria-label="blog" title="blog">Blogs</a>
+        <a className={not.error_page__button} href="/careers" aria-label="services" title="services">Careers</a>
+        </div>
+        <p className={not.error_page__heading_description} style={{
+          marginLeft:"1rem",
+          marginRight:"1rem"
+        }}>
+          If you need further assistance, please mail us at: <a href="mailto:support@curiotory.com">support@curiotory.com</a>.
+        </p>
       </div>
     </div>
   </div>
